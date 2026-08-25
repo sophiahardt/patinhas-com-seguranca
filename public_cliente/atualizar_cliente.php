cc35721c7c80991a712c9ca00fcc2e2e0ac63ed6
sophia <sophia_hardt@estudante.sesisenai.org.br>
2026-08-25 11:14:33 -0300

    add - adicionei a funcionalidade de atualizar clientes

---
+++ public_cliente/atualizar_cliente.php
@@ -0,0 +1,29 @@
+<?php
+
+include "../infra/conexao.php";
+
+$id = $_POST["id"];
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$telefone = $_POST["telefone"];
+$endereco = $_POST["endereco"];
+
+$sql = "UPDATE clientes SET nome = ?, email = ?, telefone = ?, endereco = ? WHERE id = ?";
+
+$stmt = $conexao->prepare($sql);
+if ($stmt === false) {
+    die("Erro na conexão!" . $conexao->error);
+}
+
+$stmt->bind_param("ssssi", $nome, $email, $telefone, $endereco, $id);
+$stmt->execute();
+
+if ($stmt->execute()) {
+    echo "Cliente atualizado com sucesso!";
+} else {
+    echo "Erro ao atualizar cliente: " . $stmt->error;
+}
+
+$stmt->close();
+
+?>
