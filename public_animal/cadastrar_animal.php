de98f44e281a23d88d788389c44682ffd0346fa7
sophia <sophia_hardt@estudante.sesisenai.org.br>
2026-08-26 11:26:08 -0300

    add - alterei o cadastro de animais

--- public_animal/cadastrar_animal.php
+++ public_animal/cadastrar_animal.php
@@ -7,14 +7,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $especie = $_POST["especie"];
     $raca = $_POST["raca"];
     $idade = $_POST["idade"];
+    $cliente_id = $_POST["cliente_id"];
 
-    $sql = "INSERT INTO animais (nome, especie, raca, idade) VALUES (?, ?, ?, ?)";
+    $sql = "INSERT INTO animais (nome, especie, raca, idade, cliente_id) VALUES (?, ?, ?, ?, ?)";
 
     $stmt = $conexao->prepare($sql);
     if ($stmt === false) {
         die("Erro ao preparar a consulta: " . $conexao->error);
     }
-    $stmt->bind_param("sssi", $nome, $especie, $raca, $idade);
+    $stmt->bind_param("sssi", $nome, $especie, $raca, $idade, $cliente_id);
 
     if ($stmt->execute()) {
         header("Location: ../index.php");
