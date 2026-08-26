8ab821774e7c325c6de7abec8af11b9c11ba00b1
sophia <sophia_hardt@estudante.sesisenai.org.br>
2026-08-26 11:11:51 -0300

    add - adicionei cadastro de cliente, e tabelas

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <?php
 include "../infra/conexao.php";
 
-$sql  = "SELECT animais.*, clientes.nome 
-FROM animais
-INNER JOIN clientes 
-ON animais.cliente_id = clientes.id";
+$sqlClientes = "SELECT * FROM clientes";
+$consultaClientes = $conexao->prepare($sqlClientes);
+$consultaClientes->execute();
+$clientes = $consultaClientes->get_result();
 
-$consulta = $conexao->prepare($sql);
-$consulta->execute();
-$animais = $consulta->get_result();
+$sqlAnimais = "SELECT animais.*, clientes.nome AS nome_cliente
+               FROM animais
+               INNER JOIN clientes
+               ON animais.cliente_id = clientes.id";
+
+$consultaAnimais = $conexao->prepare($sqlAnimais);
+$consultaAnimais->execute();
+$animais = $consultaAnimais->get_result();
 
 ?>
 
@@ -29,39 +34,93 @@ $animais = $consulta->get_result();
         <h2>Cadastre um cliente</h2>
         <form action="public/cadastrar_cliente.php" method="POST">
             <label for="nome">Nome:</label>
-            <input type="text" name="nome">
+            <input type="text" name="nome" required>
             <br>
             <label for="email">Email:</label>
-            <input type="text" name="email">
+            <input type="text" name="email" required>
             <br>
             <label for="telefone">Telefone:</label>
-            <input type="text" name="telefone">
+            <input type="text" name="telefone" required>
             <br>
             <label for="endereco">Endereço:</label>
-            <input type="text" name="endereco">
+            <input type="text" name="endereco" required>
             <br>
             <button type="submit">Cadastrar</button>
         </form>
 
-        <?php
+        <h2>Clientes cadastrados</h2>
 
-        $sqlUsuarios = "SELECT * FROM usuarios";
-        $consultaUsuarios = $conexao->prepare($sqlUsuarios);
-        $consultaUsuarios->execute();
-        $usuarios = $consultaUsuarios->get_result();
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Telefone</th>
+                <th>Endereço</th>
+                <th>Ações</th>
+            </tr>
 
-        while ($usuario = $usuarios->fetch_assoc()) {
-            echo "<option value='{$usuario["id_usuario"]}'>{$usuario["nome_usuario"]}</option>";
-        }
-        ?>
+            <?php while ($cliente = $clientes->fetch_assoc()) { ?>
 
-        </select>
+            <tr>
+                <td><?php echo $cliente["id"]; ?></td>
+                <td><?php echo $cliente["nome"]; ?></td>
+                <td><?php echo $cliente["email"]; ?></td>
+                <td><?php echo $cliente["telefone"]; ?></td>
+                <td><?php echo $cliente["endereco"]; ?></td>
 
-        <br>
+                <td>
+                    <a href="public/editar_clientes.php?id_cliente=<?php echo $cliente["id"]; ?>">
+                        Editar
+                    </a>
+                    <a href="public/excluir_clientes.php?id_cliente=<?php echo $cliente["id"]; ?>">
+                        Excluir
+                    </a>
+                </td>
+            </tr>
 
-        <button type="submit">Cadastrar</button>
-
+            <?php } ?>
+                    
+        </table>
         
+
+        <h2>Animais cadastrados</h2>
+
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Espécie</th>
+                <th>Raça</th>
+                <th>Idade</th>
+                <th>Responsável</th>
+                <th>Ações</th>
+            </tr>
+
+            <?php while ($animal = $animais->fetch_assoc()) { ?>
+
+            <tr>
+
+                <td><?php echo $animal["id"]; ?></td>
+                <td><?php echo $animal["nome"]; ?></td>
+                <td><?php echo $animal["especie"]; ?></td>
+                <td><?php echo $animal["raca"]; ?></td>
+                <td><?php echo $animal["idade"]; ?></td>
+                <td><?php echo $animal["nome_cliente"]; ?></td>
+        
+                <td>
+                    <a href="public/editar_animais.php?id_animal=<?php echo $animal["id"]; ?>">
+                        Editar
+                    </a>
+                    <a href="public/excluir_animais.php?id_animal=<?php echo $animal["id"]; ?>">
+                        Excluir
+                    </a>
+                </td>
+            </tr>
+        
+            <?php } ?>
+        </table>
+
 
     </main>
 </body>
